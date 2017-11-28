5171afdbd2deff5849c8aa55f8127b21114fd2a5
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-28 14:28:03 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,66 @@
     </div>
 	</section>
+
+<div id="containerfooter" class="footerclass" itemscope itemtype="http://schema.org/WPFooter">
+  <div class="container">
+  	<div class="row">
+  		<?php global $virtue; if(isset($virtue['footer_layout'])) { $footer_layout = $virtue['footer_layout']; } else { $footer_layout = 'fourc'; }
+  			if ($footer_layout == "fourc") {
+  				if (is_active_sidebar('footer_1') ) { ?> 
+					<div class="col-md-3 col-sm-6 footercol1">
+					<?php dynamic_sidebar('footer_1'); ?>
+					</div> 
+            	<?php }; ?>
+				<?php if (is_active_sidebar('footer_2') ) { ?> 
+					<div class="col-md-3  col-sm-6 footercol2">
+					<?php dynamic_sidebar('footer_2'); ?>
+					</div> 
+		        <?php }; ?>
+		        <?php if (is_active_sidebar('footer_3') ) { ?> 
+					<div class="col-md-3 col-sm-6 footercol3">
+					<?php dynamic_sidebar('footer_3'); ?>
+					</div> 
+	            <?php }; ?>
+				<?php if (is_active_sidebar('footer_4') ) { ?> 
+					<div class="col-md-3 col-sm-6 footercol4">
+					<?php dynamic_sidebar('footer_4'); ?>
+					</div> 
+		        <?php }; ?>
+		    <?php } else if($footer_layout == "threec") {
+		    	if (is_active_sidebar('footer_third_1') ) { ?> 
+					<div class="col-md-4 footercol1">
+					<?php dynamic_sidebar('footer_third_1'); ?>
+					</div> 
+            	<?php }; ?>
+				<?php if (is_active_sidebar('footer_third_2') ) { ?> 
+					<div class="col-md-4 footercol2">
+					<?php dynamic_sidebar('footer_third_2'); ?>
+					</div> 
+		        <?php }; ?>
+		        <?php if (is_active_sidebar('footer_third_3') ) { ?> 
+					<div class="col-md-4 footercol3">
+					<?php dynamic_sidebar('footer_third_3'); ?>
+					</div> 
+	            <?php }; ?>
+			<?php } else {
+					if (is_active_sidebar('footer_double_1') ) { ?>
+					<div class="col-md-6 footercol1">
+					<?php dynamic_sidebar('footer_double_1'); ?> 
+					</div> 
+		            <?php }; ?>
+		        <?php if (is_active_sidebar('footer_double_2') ) { ?>
+					<div class="col-md-6 footercol2">
+					<?php dynamic_sidebar('footer_double_2'); ?> 
+					</div> 
+		            <?php }; ?>
+		        <?php } ?>
+        </div>
+
+  </div>
+
+</div>
+</div><!--Wrapper-->
+
 	<footer class="main-footer clearfix" role="contentinfo">
 		<div class="main-footer__inner">
 			<div class="region-footer">
@@ -63,65 +124,5 @@
 			</div>
 		</div>
 	</footer>
-
-<div id="containerfooter" class="footerclass" itemscope itemtype="http://schema.org/WPFooter">
-  <div class="container">
-  	<div class="row">
-  		<?php global $virtue; if(isset($virtue['footer_layout'])) { $footer_layout = $virtue['footer_layout']; } else { $footer_layout = 'fourc'; }
-  			if ($footer_layout == "fourc") {
-  				if (is_active_sidebar('footer_1') ) { ?> 
-					<div class="col-md-3 col-sm-6 footercol1">
-					<?php dynamic_sidebar('footer_1'); ?>
-					</div> 
-            	<?php }; ?>
-				<?php if (is_active_sidebar('footer_2') ) { ?> 
-					<div class="col-md-3  col-sm-6 footercol2">
-					<?php dynamic_sidebar('footer_2'); ?>
-					</div> 
-		        <?php }; ?>
-		        <?php if (is_active_sidebar('footer_3') ) { ?> 
-					<div class="col-md-3 col-sm-6 footercol3">
-					<?php dynamic_sidebar('footer_3'); ?>
-					</div> 
-	            <?php }; ?>
-				<?php if (is_active_sidebar('footer_4') ) { ?> 
-					<div class="col-md-3 col-sm-6 footercol4">
-					<?php dynamic_sidebar('footer_4'); ?>
-					</div> 
-		        <?php }; ?>
-		    <?php } else if($footer_layout == "threec") {
-		    	if (is_active_sidebar('footer_third_1') ) { ?> 
-					<div class="col-md-4 footercol1">
-					<?php dynamic_sidebar('footer_third_1'); ?>
-					</div> 
-            	<?php }; ?>
-				<?php if (is_active_sidebar('footer_third_2') ) { ?> 
-					<div class="col-md-4 footercol2">
-					<?php dynamic_sidebar('footer_third_2'); ?>
-					</div> 
-		        <?php }; ?>
-		        <?php if (is_active_sidebar('footer_third_3') ) { ?> 
-					<div class="col-md-4 footercol3">
-					<?php dynamic_sidebar('footer_third_3'); ?>
-					</div> 
-	            <?php }; ?>
-			<?php } else {
-					if (is_active_sidebar('footer_double_1') ) { ?>
-					<div class="col-md-6 footercol1">
-					<?php dynamic_sidebar('footer_double_1'); ?> 
-					</div> 
-		            <?php }; ?>
-		        <?php if (is_active_sidebar('footer_double_2') ) { ?>
-					<div class="col-md-6 footercol2">
-					<?php dynamic_sidebar('footer_double_2'); ?> 
-					</div> 
-		            <?php }; ?>
-		        <?php } ?>
-        </div>
-
-  </div>
-
-</div>
-</div><!--Wrapper-->
   <script src="https://www.epa.gov/sites/all/modules/custom/epa_core/js/alert.js"></script>
 <?php wp_footer(); ?>
