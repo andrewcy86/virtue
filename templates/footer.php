036590e8b434fd5dffb5d1ba79882f4cf4a24532
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-28 10:52:20 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,73 @@
-<footer id="containerfooter" class="footerclass" itemscope itemtype="http://schema.org/WPFooter">
+       </div>
+        </div>
+      </div>
+    </div>
+	</section>
+	<footer class="main-footer clearfix" role="contentinfo">
+		<div class="main-footer__inner">
+			<div class="region-footer">
+				<div class="block-pane-epa-global-footer" id="block-pane-epa-global-footer">
+					<div class="row cols-3">
+						<div class="col size-1of3">
+							<div class="col__title">
+								Discover.
+							</div>
+							<ul class="menu">
+								<li><a href="https://www.epa.gov/accessibility">Accessibility</a></li>
+								<li><a href="https://www.epa.gov/aboutepa/epas-administrator">EPA Administrator</a></li>
+								<li><a href="https://www.epa.gov/planandbudget">Budget &amp; Performance</a></li>
+								<li><a href="https://www.epa.gov/contracts">Contracting</a></li>
+								<li><a href="https://www.epa.gov/home/grants-and-other-funding-opportunities">Grants</a></li>
+								<li><a href="https://19january2017snapshot.epa.gov">January 19, 2017 Web Snapshot</a></li>
+								<li><a href="https://www.epa.gov/ocr/whistleblower-protections-epa-and-how-they-relate-non-disclosure-agreements-signed-epa-employees">No FEAR Act Data</a></li>
+								<li><a href="https://www.epa.gov/privacy">Privacy</a></li>
+							</ul>
+						</div>
+						<div class="col size-1of3">
+							<div class="col__title">
+								Connect.
+							</div>
+							<ul class="menu">
+								<li><a href="https://www.data.gov/">Data.gov</a></li>
+								<li><a href="https://www.epa.gov/office-inspector-general/about-epas-office-inspector-general">Inspector General</a></li>
+								<li><a href="https://www.epa.gov/careers">Jobs</a></li>
+								<li><a href="https://www.epa.gov/newsroom">Newsroom</a></li>
+								<li><a href="https://www.epa.gov/open">Open Government</a></li>
+								<li><a href="https://www.regulations.gov/">Regulations.gov</a></li>
+								<li><a href="https://www.epa.gov/newsroom/email-subscriptions">Subscribe</a></li>
+								<li><a href="https://www.usa.gov/">USA.gov</a></li>
+								<li><a href="https://www.whitehouse.gov/">White House</a></li>
+							</ul>
+						</div>
+						<div class="col size-1of3">
+							<div class="col__title">
+								Ask.
+							</div>
+							<ul class="menu">
+								<li><a href="https://www.epa.gov/home/forms/contact-us">Contact Us</a></li>
+								<li><a href="https://www.epa.gov/home/epa-hotlines">Hotlines</a></li>
+								<li><a href="https://www.epa.gov/foia">FOIA Requests</a></li>
+								<li><a href="https://www.epa.gov/home/frequent-questions-specific-epa-programstopics">Frequent Questions</a></li>
+							</ul>
+							<div class="col__title">
+								Follow.
+							</div>
+							<ul class="social-menu">
+								<li><a class="menu-link social-facebook" href="https://www.facebook.com/EPA">Facebook</a></li>
+								<li><a class="menu-link social-twitter" href="https://twitter.com/epa">Twitter</a></li>
+								<li><a class="menu-link social-youtube" href="https://www.youtube.com/user/USEPAgov">YouTube</a></li>
+								<li><a class="menu-link social-flickr" href="https://www.flickr.com/photos/usepagov">Flickr</a></li>
+								<li><a class="menu-link social-instagram" href="https://www.instagram.com/epagov">Instagram</a></li>
+							</ul>
+							<p class="last-updated">Last updated on {Month day, YYYY}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+<div id="containerfooter" class="footerclass" itemscope itemtype="http://schema.org/WPFooter">
   <div class="container">
   	<div class="row">
   		<?php global $virtue; if(isset($virtue['footer_layout'])) { $footer_layout = $virtue['footer_layout']; } else { $footer_layout = 'fourc'; }
@@ -52,24 +121,10 @@
 		            <?php }; ?>
 		        <?php } ?>
         </div>
-        <div class="footercredits clearfix">
-    		
-    		<?php if (has_nav_menu('footer_navigation')) :
-        	?><div class="footernav clearfix"><?php 
-              wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => 'footermenu'));
-            ?></div><?php
-        	endif;?>
-        	<p><?php if(isset($virtue['footer_text'])) { $footertext = $virtue['footer_text'];} else {$footertext = '[copyright] [the-year] [site-name] [theme-credit]';}
-        		$footertext = str_replace('[copyright]','&copy;',$footertext);
-        		$footertext = str_replace('[the-year]',date('Y'),$footertext);
-        		$footertext = str_replace('[site-name]',get_bloginfo('name'),$footertext);
-        		$footertext = str_replace('[theme-credit]','- WordPress Theme by <a href="https://www.kadencethemes.com/" target="_blank">Kadence Themes</a>',$footertext);
-        		 echo do_shortcode($footertext); ?></p>
-    	</div>
 
   </div>
 
-</footer>
+</div>
 </div><!--Wrapper-->
-
+  <script src="https://www.epa.gov/sites/all/modules/custom/epa_core/js/alert.js"></script>
 <?php wp_footer(); ?>
