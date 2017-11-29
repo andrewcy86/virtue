79460c128f3507420a93d78298d00dc62fca8065
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-29 11:40:22 -0500

    Update content.php

--- templates/content.php
+++ templates/content.php
@@ -6,94 +6,6 @@ $cat_id = $cat_id_array[0]->cat_ID;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope="" itemtype="http://schema.org/BlogPosting">
 <div class="row">
-    
-    <?php 
-$edgID = get_post_meta($post->ID, 'EDG_ID', true);
-if ($edgID && $cat_id == 2 && is_single()) {
- $curl = curl_init();
-      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($curl, CURLOPT_URL, 'https://edg.epa.gov/metadata/rest/find/document?f=dcat&uuid='. $edgID);
-      $content = curl_exec($curl);
-	  curl_close($curl);
-$json = json_decode(utf8_encode($content));
-// BEGIN JSON ERROR CHECK
-if ($json === null
-    || json_last_error() !== JSON_ERROR_NONE) {
-    echo "<span class='warning'>Error retrieving information. Please check back later.</span>";
-} else { 
-?>
-<?php
-//$i=0;
-//foreach($json->dataset as $item) {
-//	     if($i!=0)  
-//        break;
-//    echo '<header class="entry-header"><h2 class="entry-title">' . $item->title . '</h2></header>';
-//	$i++;
-//}
-?>
-	
-<?php 
-echo '<div class="entry-content">';
-$i_link=0;
-foreach($json->dataset as $item) { 
-if($i_link!=0) 
-	break;
-   $arr = $item->distribution;
-   $result = count($arr);
-   //echo $result;
-   $default = '<div class="post-format"><div class="format-container pad"><p><a href="https://edg.epa.gov/metadata/catalog/search/resource/details.page?uuid='. $edgID .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Additional Information &rarr;</a></p></div></div>';
-   if ($result == 0) {
-    echo  $default;
-} else if ($result == 1) {
-    echo  $default;
-	echo '<div class="post-format"><i class="fa fa-link fa-2x"></i><div class="format-container pad"><p><a href="'. $arr[0]->downloadURL .'" class="post-format-link" target="_blank">Download Data &rarr;</a></p></div></div>';
-} else if ($result == 2) {
-	
-	if ($arr[0]->accessURL != null && $arr[1]->accessURL != null ) {
-    echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
-	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
-	} else if ($arr[0]->downloadURL != null && $arr[1]->downloadURL != null) {
-	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
-	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
-	} else if ($arr[0]->accessURL != null && $arr[1]->downloadURL != nulll) {
-	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
-	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
-	} else {
-	echo  $default;	
-	}
-} else {
-	echo  $default;	
-	}
-	$i_link++;
-}
-?>
-
-
-						
-						
-<?php 
-$i_contact=0;
-foreach($json->dataset as $item) {
-if($i_contact!=0) 
-	break;	   
-    echo $item->description;
-	echo '<br /><br /><strong>Organization Name</strong>: ' . $item->publisher->name;
-	echo '<br /><br /><strong>Contact</strong>: <a href="mailto:'.str_replace("mailto:", "", $item->contactPoint->hasEmail).'">' . $item->contactPoint->fn .'</a> ('. str_replace("mailto:", "", $item->contactPoint->hasEmail) .')<br /><br />';
-	echo '</div><!-- .entry-content -->';
-$i_contact++;
-}
-?>
-
-<?php
-} // END JSON ERROR CHECK
-?>
-
-						
-<?php 
-} else {
-?>
-
         <?php global $post, $virtue; 
             $postsummery  = get_post_meta( $post->ID, '_kad_post_summery', true );
             $height       = get_post_meta( $post->ID, '_kad_posthead_height', true );
@@ -270,6 +182,94 @@ $i_contact++;
                 $textsize = 'col-md-12 kttextpost'; 
             } ?>
 
+	 <?php 
+$edgID = get_post_meta($post->ID, 'EDG_ID', true);
+if ($edgID && $cat_id == 2 && is_single()) {
+ $curl = curl_init();
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_URL, 'https://edg.epa.gov/metadata/rest/find/document?f=dcat&uuid='. $edgID);
+      $content = curl_exec($curl);
+	  curl_close($curl);
+$json = json_decode(utf8_encode($content));
+// BEGIN JSON ERROR CHECK
+if ($json === null
+    || json_last_error() !== JSON_ERROR_NONE) {
+    echo "<span class='warning'>Error retrieving information. Please check back later.</span>";
+} else { 
+?>
+<?php
+//$i=0;
+//foreach($json->dataset as $item) {
+//	     if($i!=0)  
+//        break;
+//    echo '<header class="entry-header"><h2 class="entry-title">' . $item->title . '</h2></header>';
+//	$i++;
+//}
+?>
+	
+<?php 
+echo '<div class="entry-content">';
+$i_link=0;
+foreach($json->dataset as $item) { 
+if($i_link!=0) 
+	break;
+   $arr = $item->distribution;
+   $result = count($arr);
+   //echo $result;
+   $default = '<div class="post-format"><div class="format-container pad"><p><a href="https://edg.epa.gov/metadata/catalog/search/resource/details.page?uuid='. $edgID .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Additional Information &rarr;</a></p></div></div>';
+   if ($result == 0) {
+    echo  $default;
+} else if ($result == 1) {
+    echo  $default;
+	echo '<div class="post-format"><i class="fa fa-link fa-2x"></i><div class="format-container pad"><p><a href="'. $arr[0]->downloadURL .'" class="post-format-link" target="_blank">Download Data &rarr;</a></p></div></div>';
+} else if ($result == 2) {
+	
+	if ($arr[0]->accessURL != null && $arr[1]->accessURL != null ) {
+    echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
+	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
+	} else if ($arr[0]->downloadURL != null && $arr[1]->downloadURL != null) {
+	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
+	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
+	} else if ($arr[0]->accessURL != null && $arr[1]->downloadURL != nulll) {
+	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[0]->accessURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Access Data &rarr;</a></p></div></div>';
+	echo '<div class="post-format"><div class="format-container pad"><p><a href="'. $arr[1]->downloadURL .'" class="post-format-link" target="_blank"><i class="fa fa-link fa-2x"></i> Download Data &rarr;</a></p></div></div>';
+	} else {
+	echo  $default;	
+	}
+} else {
+	echo  $default;	
+	}
+	$i_link++;
+}
+?>
+
+
+						
+						
+<?php 
+$i_contact=0;
+foreach($json->dataset as $item) {
+if($i_contact!=0) 
+	break;	   
+    echo $item->description;
+	echo '<br /><br /><strong>Organization Name</strong>: ' . $item->publisher->name;
+	echo '<br /><br /><strong>Contact</strong>: <a href="mailto:'.str_replace("mailto:", "", $item->contactPoint->hasEmail).'">' . $item->contactPoint->fn .'</a> ('. str_replace("mailto:", "", $item->contactPoint->hasEmail) .')<br /><br />';
+	echo '</div><!-- .entry-content -->';
+$i_contact++;
+}
+?>
+
+<?php
+} // END JSON ERROR CHECK
+?>
+
+						
+<?php 
+} else {
+?>
+
+	
             <div class="<?php echo esc_attr($textsize);?> post-text-container postcontent">
                 <?php get_template_part('templates/post', 'date'); ?> 
                 <header>
